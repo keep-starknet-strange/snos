d36712ea338919a599bd73826529dd23023f5002
Ben Goebel <dontpanicdao@gmail.com>
2023-09-21 02:06:54 -0600

    full pie encoding

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,5 +1,11 @@
 #[derive(thiserror::Error, Clone, Debug)]
 pub enum SnOsError {
-    #[error("SnOs Error: {0}.")]
+    #[error("SnOs Error: {0}")]
     CatchAll(String),
+    #[error("PIE Parse Error: {0}")]
+    PieParsing(String),
+    #[error("PIE Zip Error: {0}")]
+    PieZipping(String),
+    #[error("PIE Encode Error: {0}")]
+    PieEncoding(String),
 }
--- src/pie.rs
+++ src/pie.rs
@@ -1,100 +1,86 @@
-use cairo_vm::types::relocatable::MaybeRelocatable;
-use num_bigint::BigUint;
-use num_traits::Num;
+use crate::error::SnOsError;
+use base64::{engine::general_purpose, Engine as _};
+use cairo_vm::vm::runners::cairo_pie::CairoPie;
 use std::fs::File;
-use std::io::{Read, Write};
-use std::path::PathBuf;
+use std::io::{Cursor, Read, Seek, Write};
+use std::path::Path;
 use zip::write::FileOptions;
-
-use cairo_vm::vm::runners::cairo_pie::CairoPieMemory;
-
-const _CAIRO_PIE_VERSION: &str = "1.1";
-
-const ADDR_BYTE_LEN: usize = 8;
-const FIELD_BYTE_LEN: usize = 32;
-const _SEGMENT_BIT_LEN: usize = 16;
-const _OFFSET_BIT_LEN: usize = 47;
-const ADDR_BASE: usize = 0x8000000000000000; // 2 ** (8 * ADDR_BYTE_LEN - 1)
-const OFFSET_BASE: usize = 0x800000000000; // 2 ** OFFSET_BIT_LEN
-const RELOCATE_BASE: &str = "8000000000000000000000000000000000000000000000000000000000000000"; // 2 ** (8 * FIELD_BYTE_LEN - 1)
+use zip::ZipWriter;
 
 const PIE_FILES: [&'static str; 5] = [
-    "metadata.json",
-    "memory.bin",
-    "additional_data.json",
-    "execution_resources.json",
-    "version.json",
+    "metadata",
+    "memory",
+    "additional_data",
+    "execution_resources",
+    "version",
 ];
 
-// Memory address in Cairo is represented like this: <segment>:<offset>
-// <segment> is the segment number deterimined by runner
-// <offset> is the position of the address relative to the segment
-// "relocatable": on program run these memory cells will be relocated so that the memory ends up continuous.
-// field bytes = 32
+pub fn zip_pie(pie: CairoPie, dst: &Path) -> Result<(), SnOsError> {
+    let output = File::create(dst).map_err(|e| SnOsError::PieZipping(format!("{e}")))?;
 
-// Serializes RelocatableValue as:
-// 1bit |   SEGMENT_BITS |   OFFSET_BITS
-// 1    |     segment    |   offset
-// Serializes int as
-// 1bit | num
-// 0    | num
+    let zip = zip::ZipWriter::new(output);
 
-pub fn serialize_memory(memory: CairoPieMemory) -> Vec<u8> {
-    let mem_cap = memory.len() * ADDR_BYTE_LEN + memory.len() * FIELD_BYTE_LEN;
-    let mut res = Vec::with_capacity(mem_cap);
+    write_to_zip(pie, zip)?;
 
-    // TODO: safety checks
-    for ((segment, offset), value) in memory.iter() {
-        match value {
-            MaybeRelocatable::RelocatableValue(rel_val) => {
-                let mem_addr = ADDR_BASE + *segment * OFFSET_BASE + *offset;
-
-                let reloc_base = BigUint::from_str_radix(RELOCATE_BASE, 16).unwrap();
-                let reloc_value = reloc_base
-                    + BigUint::from(rel_val.segment_index as usize) * BigUint::from(OFFSET_BASE)
-                    + BigUint::from(rel_val.offset);
-                res.extend_from_slice(mem_addr.to_le_bytes().as_ref());
-                res.extend_from_slice(reloc_value.to_bytes_le().as_ref());
-            }
-            MaybeRelocatable::Int(data_val) => {
-                let mem_addr = ADDR_BASE + *segment * OFFSET_BASE + *offset;
-                res.extend_from_slice(mem_addr.to_le_bytes().as_ref());
-                res.extend_from_slice(data_val.to_le_bytes().as_ref());
-            }
-        };
-    }
-
-    res
+    Ok(())
 }
 
-pub fn zip_pie(path: &str) {
-    let mut pie_dir = PathBuf::from(path.clone());
-    let pie_zip = PathBuf::from(path).join("testy.zip");
+pub fn zip_pie_mem(pie: CairoPie) -> Result<Vec<u8>, SnOsError> {
+    let mut data = Vec::new();
 
-    let file = File::create(pie_zip).unwrap();
+    {
+        let buf = Cursor::new(&mut data);
 
-    let mut zip = zip::ZipWriter::new(file);
+        let zip = zip::ZipWriter::new(buf);
 
+        write_to_zip(pie, zip)?;
+    }
+
+    Ok(data)
+}
+
+pub fn encode_pie(dst: &Path) -> Result<String, SnOsError> {
+    let mut pie_zip = File::open(dst).map_err(|e| SnOsError::PieEncoding(format!("{e}")))?;
+    let mut buffer = Vec::new();
+
+    // Read file into vector.
+    pie_zip
+        .read_to_end(&mut buffer)
+        .map_err(|e| SnOsError::PieEncoding(format!("{e}")))?;
+
+    Ok(general_purpose::STANDARD_NO_PAD.encode(buffer))
+}
+
+pub fn encode_pie_mem(buffer: Vec<u8>) -> Result<String, SnOsError> {
+    Ok(general_purpose::STANDARD_NO_PAD.encode(buffer))
+}
+
+fn write_to_zip<W: Write + Seek>(pie: CairoPie, mut zip: ZipWriter<W>) -> Result<(), SnOsError> {
     let options = FileOptions::default()
         .compression_method(zip::CompressionMethod::Deflated)
         .unix_permissions(0o755);
 
-    let mut buffer = Vec::new();
+    let pie_s = serde_json::to_value(&pie).map_err(|e| SnOsError::PieZipping(format!("{e}")))?;
+
     for file in PIE_FILES {
-        pie_dir.push(file);
-        if !pie_dir.exists() {
-            println!("PATH DOES NOT EXIST: ");
-        }
-        let _ = zip.start_file(file, options);
-        let mut f = File::open(pie_dir.clone()).unwrap();
-        f.read_to_end(&mut buffer).unwrap();
-        zip.write_all(&buffer).unwrap();
-        buffer.clear();
-        pie_dir.pop();
+        if file == "memory" {
+            let pie_mem_bytes = hex::decode(&pie_s[file].to_string().trim_matches('"'))
+                .map_err(|e| SnOsError::PieZipping(format!("{e}")))?;
+
+            zip.start_file(&format!("{file}.bin"), options)
+                .map_err(|e| SnOsError::PieZipping(format!("{e}")))?;
+            zip.write_all(&pie_mem_bytes)
+                .map_err(|e| SnOsError::PieZipping(format!("{e}")))?;
+        } else {
+            zip.start_file(&format!("{file}.json"), options)
+                .map_err(|e| SnOsError::PieZipping(format!("{e}")))?;
+            zip.write_all(pie_s[file].to_string().as_bytes())
+                .map_err(|e| SnOsError::PieZipping(format!("{e}")))?;
+        };
     }
 
-    zip.finish().unwrap();
-}
+    zip.finish()
+        .map_err(|e| SnOsError::PieZipping(format!("{e}")))?;
 
-// "add_job", {"cairo_pie": base64.b64encode(cairo_pie.serialize()).decode("ascii")}
-pub fn encode_pie() {}
+    Ok(())
+}
