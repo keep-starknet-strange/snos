7754ced26d368332f933f8e77109f6eaa99b3ea3
Evolve <artevolve@yahoo.com>
2023-09-21 13:55:16 +0200

    ✨ add more fields + todo

--- src/os_input.rs
+++ src/os_input.rs
@@ -1,10 +1,15 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 use starknet::core::types::FieldElement;
 
 use crate::storage::starknet::CommitmentInfo;
 
+// TODO: Add missing fields
 #[derive(Serialize, Deserialize)]
 struct StarknetOsInput {
     contract_state_commitment_info: CommitmentInfo,
+    contract_class_commitment_info: CommitmentInfo,
+    class_hash_to_compiled_class_hash: HashMap<FieldElement, FieldElement>,
     block_hash: FieldElement,
 }
