5cf3054c69c70e72f1ce704424b170474d30aff4
LucasLvy <lucaslevy10@gmail.com>
2023-10-17 16:47:31 +0200

    add cairo 1 contracts

--- scripts/setup-tests.sh
+++ scripts/setup-tests.sh
@@ -14,6 +14,13 @@ then
     exit 1
 fi
 
+if ! command -v starknet-compile > /dev/null
+then
+    echo "please install scarb https://docs.swmansion.com/scarb/download.html#install-via-installation-script"
+    echo "curl --proto '=https' --tlsv1.2 -sSf https://docs.swmansion.com/scarb/install.sh | sh"
+    exit 1
+fi
+
 echo -e "\ninitializing cairo-lang($CAIRO_VER)...\n"
 git submodule update --init
 
@@ -40,7 +47,7 @@ cp tests/dependencies/initializable.cairo cairo-lang/src/starkware/starknet/std_
 
 
 # compile cairo programs
-echo -e "compmiling cairo programs...\n"
+echo -e "compiling cairo programs...\n"
 mkdir -p build/programs
 cairo-format -i tests/programs/*
 cairo-compile tests/programs/bad_output.cairo --output build/programs/bad_output.json
@@ -51,7 +58,7 @@ cairo-compile tests/programs/load_deprecated_class.cairo --output build/programs
 cairo-compile cairo-lang/src/starkware/starknet/core/os/os.cairo --output build/os_debug.json --cairo_path cairo-lang/src
 
 # compile starknet contract
-echo -e "compiling starknet contrarcts...\n"
+echo -e "compiling starknet contracts...\n"
 mkdir -p build/contracts
 ln -s cairo-lang/src/starkware starkware
 starknet-compile-deprecated --no_debug_info tests/contracts/token_for_testing.cairo --output build/contracts/token_for_testing.json --cairo_path cairo-lang/src --account_contract
@@ -60,3 +67,4 @@ starknet-compile-deprecated --no_debug_info tests/contracts/dummy_token.cairo --
 starknet-compile-deprecated --no_debug_info tests/contracts/delegate_proxy.cairo --output build/contracts/delegate_proxy.json --cairo_path cairo-lang/src
 starknet-compile-deprecated --no_debug_info tests/contracts/test_contract.cairo --output build/contracts/test_contract.json --cairo_path cairo-lang/src
 starknet-compile-deprecated --no_debug_info tests/contracts/test_contract2.cairo --output build/contracts/test_contract2.json --cairo_path cairo-lang/src
+cd ./tests/contracts/cairo1/ && scarb build && mv target/dev/* ../../../
---
+++ tests/contracts/cairo1/src/lib.cairo
@@ -0,0 +1,4 @@
+mod no_validate_account;
+mod counter;
+use no_validate_account::Account;
+use counter::HelloStarknet;
