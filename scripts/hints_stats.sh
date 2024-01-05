31b667b969f01f33e94ef1c3177c80228b0c6767
Maciej Kamiński <aundumla@gmail.com>
2024-01-05 12:34:23 +0100

    move hints to examples

--- scripts/hints_stats.sh
+++ scripts/hints_stats.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ALL=`cargo run --quiet --bin hints all | jq length`
-EXTERNALLY=`cargo run --quiet --bin hints implemented_externally | jq length`
-SNOS=`cargo run --quiet --bin hints implemented_in_snos | jq length`
-IMPLEMENTED=`cargo run --quiet --bin hints implemented | jq length`
-UNIMPLEMENTED=`cargo run --quiet --bin hints unimplemented | jq length`
-ORPHANS=`cargo run --quiet --bin hints orphans | jq length`
+ALL=`cargo run --quiet --example hints all | jq length`
+EXTERNALLY=`cargo run --quiet --example hints implemented_externally | jq length`
+SNOS=`cargo run --quiet --example hints implemented_in_snos | jq length`
+IMPLEMENTED=`cargo run --quiet --example hints implemented | jq length`
+UNIMPLEMENTED=`cargo run --quiet --example hints unimplemented | jq length`
+ORPHANS=`cargo run --quiet --example hints orphans | jq length`
 
 echo 'Snos hints stats'
 echo 'all:' $ALL
