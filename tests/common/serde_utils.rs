be66a39edee1f69442359ee0c3141a2969c5fde7
Maciej Kamiński <aundumla@gmail.com>
2024-03-19 18:50:01 +0100

    remove unnecessary code

--- tests/common/serde_utils.rs
+++ tests/common/serde_utils.rs
@@ -29,40 +29,6 @@ impl SerializeAs<DeprecatedContractClass> for DeprecatedContractClassStr {
     }
 }
 
-// #[serde_as]
-// #[derive(Debug, Clone, Deserialize, Serialize)]
-// pub struct StarknetOsInputUtil {
-//     pub contract_state_commitment_info: CommitmentInfo,
-//     pub contract_class_commitment_info: CommitmentInfo,
-//     #[serde_as(as = "HashMap<Felt252Str, DeprecatedContractClassStr>")]
-//     pub deprecated_compiled_classes: HashMap<Felt252, DeprecatedContractClass>,
-//     #[serde_as(as = "HashMap<Felt252Str, Felt252Str>")]
-//     pub compiled_classes: HashMap<Felt252, Felt252>,
-//     #[serde_as(as = "HashMap<Felt252Str, Vec<Felt252Str>>")]
-//     pub compiled_class_visited_pcs: HashMap<Felt252, Vec<Felt252>>,
-//     #[serde_as(as = "HashMap<Felt252StrDec, _>")]
-//     pub contracts: HashMap<Felt252, ContractState>,
-//     #[serde_as(as = "HashMap<Felt252Str, Felt252Str>")]
-//     pub class_hash_to_compiled_class_hash: HashMap<Felt252, Felt252>,
-//     pub general_config: StarknetGeneralConfig,
-//     pub transactions: Vec<InternalTransaction>,
-//     #[serde_as(as = "Felt252Num")]
-//     pub block_hash: Felt252,
-// }
-//
-// impl StarknetOsInputUtil {
-//     pub fn load(path: &str) -> Self {
-//         let raw_input = fs::read_to_string(path).unwrap();
-//         serde_json::from_str(&raw_input).unwrap()
-//     }
-//     pub fn dump(&self, path: &str) -> Result<(), SnOsError> {
-//         fs::File::create(path)
-//             .unwrap()
-//             .write_all(&serde_json::to_vec(&self).unwrap())
-//             .map_err(|e| SnOsError::CatchAll(format!("{e}")))
-//     }
-// }
-
 #[serde_as]
 #[derive(Debug, Deserialize)]
 pub struct RawOsOutput(#[serde_as(as = "Vec<Felt252Num>")] pub Vec<Felt252>);
