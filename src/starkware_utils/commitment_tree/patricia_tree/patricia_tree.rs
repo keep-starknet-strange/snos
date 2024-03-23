fe38a346a55869c832955e33330c9e97236618bd
Olivier Desenfans <desenfans.olivier@gmail.com>
2024-03-23 14:27:06 +0100

    patricia_tree tests ok

--- src/starkware_utils/commitment_tree/patricia_tree/patricia_tree.rs
+++ src/starkware_utils/commitment_tree/patricia_tree/patricia_tree.rs
@@ -63,33 +63,131 @@ where
 
 #[cfg(test)]
 mod tests {
+    use std::collections::{HashSet, VecDeque};
+
+    use cairo_vm::Felt252;
+    use num_bigint::BigUint;
+    use num_traits::ToPrimitive;
+    use rand::seq::IteratorRandom;
+    use rand::Rng;
     use rstest::{fixture, rstest};
 
     use super::*;
     use crate::crypto::pedersen::PedersenHash;
+    use crate::starkware_utils::commitment_tree::base_types::{Length, NodePath};
+    use crate::starkware_utils::commitment_tree::patricia_tree::nodes::{BinaryNodeFact, EdgeNodeFact};
     use crate::storage::dict_storage::DictStorage;
+    use crate::storage::storage::Fact;
     use crate::storage::storage_utils::SimpleLeafFact;
 
-    #[fixture]
-    fn storage() -> impl Storage {
-        DictStorage::default()
-    }
+    type StorageType = DictStorage;
+    type HashFunction = PedersenHash;
+    type FFC = FactFetchingContext<StorageType, HashFunction>;
 
     #[fixture]
-    fn hash_function() -> impl HashFunctionType {
-        PedersenHash
+    fn ffc() -> FFC {
+        FactFetchingContext::<_, HashFunction>::new(DictStorage::default())
+    }
+
+    /// Binary node facts are simply the hash of the two children; edge node facts are the hash of
+    /// the bottom node fact and the path, plus the path length.
+    fn hash_preimage(preimage: &[BigUint]) -> BigUint {
+        let hash = if preimage.len() == 2 {
+            let node_fact =
+                BinaryNodeFact::new(preimage[0].to_bytes_be().to_vec(), preimage[1].to_bytes_be().to_vec()).unwrap();
+            <BinaryNodeFact as Fact<StorageType, HashFunction>>::hash(&node_fact)
+        } else {
+            let length = Length(preimage[0].to_u64().unwrap());
+            let path = NodePath(preimage[1].clone());
+            let bottom = preimage[2].to_bytes_be().to_vec();
+            let node_fact = EdgeNodeFact::new(bottom, path, length).unwrap();
+            <EdgeNodeFact as Fact<StorageType, HashFunction>>::hash(&node_fact)
+        };
+
+        BigUint::from_bytes_be(&hash)
+    }
+
+    /// Given a list of leaves and a collection of preimages, verifies that the preimages suffice to
+    /// descend to all leaves.
+    fn verify_leaves_are_reachable_from_root(root: BigUint, leaf_hashes: &[BigUint], preimages: BinaryFactDict) {
+        let mut leaves_reached = HashSet::<BigUint>::new();
+        let mut facts_to_open = VecDeque::new();
+        facts_to_open.push_back(root);
+
+        while let Some(next_fact) = facts_to_open.pop_front() {
+            if leaf_hashes.contains(&next_fact) {
+                leaves_reached.insert(next_fact);
+                continue;
+            }
+
+            let preimage = preimages.get(&next_fact).unwrap();
+            if preimage.len() == 3 {
+                // Edge node. Next fact is the third entry.
+                facts_to_open.push_back(preimage[2].clone());
+            } else {
+                let left_child = &preimage[0];
+                let right_child = &preimage[1];
+                facts_to_open.push_back(left_child.clone());
+                facts_to_open.push_back(right_child.clone());
+            }
+        }
+
+        let leaves_to_reach: HashSet<_> = leaf_hashes.iter().cloned().collect();
+        assert_eq!(leaves_reached, leaves_to_reach);
     }
 
     #[rstest]
     #[tokio::test]
-    async fn test_empty_tree() {
+    async fn test_empty_tree(mut ffc: FFC) {
         let height = Height(10);
-        let mut ffc = FactFetchingContext::<_, PedersenHash>::new(DictStorage::default());
 
         let leaf_node = SimpleLeafFact::empty();
 
         let patricia_tree = PatriciaTree::empty_tree(&mut ffc, height, leaf_node).await.unwrap();
         assert_eq!(patricia_tree.root, EMPTY_NODE_HASH);
         assert_eq!(patricia_tree.height, height);
+    }
+
+    /// Builds a Patricia tree using update(), and tests that the facts stored suffice to decommit.
+    #[rstest]
+    #[case::full_tree_small(Height(2), 1 << 2)]
+    #[case::full_tree_large(Height(10), 1 << 10)]
+    #[case::sparse_tree(Height(10), 5)]
+    #[case::dense_tree(Height(10), 1 << 9)]
+    #[tokio::test]
+    async fn test_update_and_decommit(mut ffc: FFC, #[case] height: Height, #[case] n_leaves: usize) {
+        let mut rng = rand::thread_rng();
+
+        let mut tree = PatriciaTree::empty_tree(&mut ffc, height, SimpleLeafFact::empty()).await.unwrap();
+
+        // Create some random modifications, store the facts and update the tree.
+        // Note that leaves with value 0 are not modifications (hence, range(1, ...)).
+        let leaves: Vec<_> =
+            (0..n_leaves).map(|_| rng.gen_range(1..=1000u64)).map(|x| SimpleLeafFact::new(Felt252::from(x))).collect();
+
+        let mut leaf_hashes_bytes = vec![];
+        for leaf_fact in leaves.iter() {
+            let leaf_hash = leaf_fact.set_fact(&mut ffc).await.unwrap();
+            leaf_hashes_bytes.push(leaf_hash);
+        }
+        let leaf_hashes: Vec<_> =
+            leaf_hashes_bytes.iter().map(|leaf_hash_bytes| BigUint::from_bytes_be(leaf_hash_bytes)).collect();
+        let indices: Vec<_> =
+            (0u64..1 << height.0).choose_multiple(&mut rng, n_leaves).into_iter().map(BigUint::from).collect();
+        let modifications: Vec<_> = indices.iter().cloned().zip(leaves.iter().cloned()).collect();
+        let mut preimages = Some(BinaryFactDict::new());
+
+        let tree = tree.update(&mut ffc, modifications, &mut preimages).await.unwrap();
+        let root = BigUint::from_bytes_be(&tree.root);
+
+        // Sanity check - the hash of the values should be the keys.
+        let preimages = preimages.unwrap();
+        for (fact, preimage) in preimages.iter() {
+            let preimage_hash = hash_preimage(preimage);
+            assert_eq!(fact, &preimage_hash);
+        }
+
+        // Verify that the root can be reached using the preimages, from every leaf.
+        verify_leaves_are_reachable_from_root(root, &leaf_hashes, preimages);
     }
 }
