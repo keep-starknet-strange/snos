6b23752144646ca0de67e5f386a6f1bcb9c2875a
Olivier Desenfans <desenfans.olivier@gmail.com>
2024-03-29 17:19:11 +0100

    Cleanup: SET_AP_TO_IS_REVERTED is already implemented
    This hint is already implemented as `IS_REVERTED`.

--- src/hints/unimplemented.rs
+++ src/hints/unimplemented.rs
@@ -37,10 +37,6 @@ const GET_OLD_BLOCK_NUMBER_AND_HASH: &str = indoc! {r#"
 const ASSERT_CASE_IS_RIGHT: &str = "assert case == 'right'";
 
 #[allow(unused)]
-const SET_AP_TO_IS_REVERTED: &str =
-    "memory[ap] = to_felt_or_relocatable(execution_helper.tx_execution_info.is_reverted)";
-
-#[allow(unused)]
 const WRITE_OLD_BLOCK_TO_STORAGE: &str = indoc! {r#"
 	storage = execution_helper.storage_by_address[ids.BLOCK_HASH_CONTRACT_ADDRESS]
 	storage.write(key=ids.old_block_number, value=ids.old_block_hash)"#
