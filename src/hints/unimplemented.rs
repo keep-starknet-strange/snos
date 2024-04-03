ae15289e765f7f0ea3d4c4a359d99035dae6c95c
Olivier Desenfans <olivier@moonsonglabs.com>
2024-04-03 11:07:06 +0200

    Feature: SET_AP_TO_TX_NONCE hint (#103)

--- src/hints/unimplemented.rs
+++ src/hints/unimplemented.rs
@@ -9,9 +9,6 @@ const CACHE_CONTRACT_STORAGE: &str = indoc! {r#"
 };
 
 #[allow(unused)]
-const SET_AP_TO_NONCE: &str = "memory[ap] = to_felt_or_relocatable(tx.nonce)";
-
-#[allow(unused)]
 const SET_TREE_STRUCTURE: &str = indoc! {r#"
 	from starkware.python.math_utils import div_ceil
 	onchain_data_start = ids.da_start
