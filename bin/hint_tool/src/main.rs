5c934098cfb9bc9447b974b8d395c7ac45cc0cce
Stephen Shelton <steve@brewcraft.org>
2024-04-09 10:52:49 -0600

    Use clap for CLI args in hint tool

--- bin/hint_tool/src/main.rs
+++ bin/hint_tool/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufReader, Write};
+use std::path::PathBuf;
 
 use blockifier::execution::hint_code;
 use cairo_vm::any_box;
@@ -10,8 +11,51 @@ use cairo_vm::types::exec_scope::ExecutionScopes;
 use cairo_vm::vm::errors::hint_errors::HintError;
 use cairo_vm::vm::vm_core::VirtualMachineBuilder;
 use cairo_vm::vm::vm_memory::memory_segments::MemorySegmentManager;
+use clap::Parser;
 use serde::Deserialize;
 use snos::hints::SnosHintProcessor;
+
+#[derive(Clone, Copy, Debug, Default, clap::ValueEnum, PartialEq)]
+enum HintStatus {
+    Implemented,
+    Unimplemented,
+    ImplementedExternally,
+    ImplementedLocally,
+    Orphaned,
+    #[default]
+    All,
+}
+
+#[derive(Clone, Copy, Debug, Default, clap::ValueEnum, PartialEq)]
+enum OutputType {
+    /// Print a JSON Array of hints
+    #[default]
+    Json,
+
+    /// Print the hints as Rust source code.
+    /// This helps avoid whitespace issues when matching hint strings
+    Rust,
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Subset of hints to report
+    #[arg(long)]
+    subset: Option<HintStatus>,
+
+    /// Output type
+    #[arg(long)]
+    out_type: Option<OutputType>,
+
+    /// Output file to write matched hints to
+    #[arg(long)]
+    out_file: Option<PathBuf>,
+
+    /// Input JSON file (e.g. "os_latest.json")
+    #[arg(long)]
+    #[clap(default_value = Some("../../build/os_latest.json"))]
+    in_file: PathBuf,
+}
 
 #[derive(Deserialize)]
 struct Hint {
@@ -24,25 +68,21 @@ struct Os {
 }
 
 fn main() -> std::io::Result<()> {
-    let subset = std::env::args().nth(1).expect(
-        "choose what you need: all, implemented, unimplemented, implemented_externally, implemented_in_snos, orphans",
-    );
+    let args = Args::parse();
+    let subset = args.subset.unwrap_or_default();
 
     let mut hint_processor = SnosHintProcessor::default();
     let snos_hints = hint_processor.hints();
 
-    let mut result = HashSet::new();
+    let mut result = Vec::new();
 
-    let os: Os = serde_json::from_reader(BufReader::new(File::open("build/os_latest.json")?))
+    let os: Os = serde_json::from_reader(BufReader::new(File::open(args.in_file)?))
         .expect("Failed to parse os_latest.json");
     let os_hints = os.hints.into_values().flatten().map(|h| h.code.to_string()).collect::<HashSet<_>>();
     let syscall_hints = hint_code::SYSCALL_HINTS.into_iter().map(|h| h.to_string()).collect::<HashSet<_>>();
 
     let segments = MemorySegmentManager::new();
     let mut vm = VirtualMachineBuilder::default().segments(segments).build();
-
-    let mut buf = String::new();
-    let mut count = 0;
 
     let mut known_to_hint_processor = |code: &String| {
         let r = hint_processor.execute_hint_extensive(
@@ -54,41 +94,60 @@ fn main() -> std::io::Result<()> {
         if let Err(e) = r {
             if let HintError::UnknownHint(_) = e {
                 println!("\n----- gave unknown hint error: ----- \n{}", code.clone());
-                buf.push_str(&format!("pub const UNUSED_HINT_{}", count));
-                buf.push_str(": &str = indoc! {r#\"");
-                buf.push_str(code.as_str());
-                buf.push_str("\"#};\n\n");
-                count += 1;
                 return false;
             }
         }
         true
     };
 
-    if subset == "orphans" {
+
+    if subset == HintStatus::Orphaned {
         for code in snos_hints.iter() {
             if !os_hints.contains(code) && !syscall_hints.contains(code) {
-                result.insert(code);
+                result.push(code);
             }
         }
     } else {
         for code in os_hints.union(&syscall_hints).collect::<HashSet<_>>() {
-            if subset == "all"
-                || subset == "implemented" && known_to_hint_processor(code)
-                || subset == "unimplemented" && !known_to_hint_processor(code)
-                || subset == "implemented_externally" && (known_to_hint_processor(code) && !snos_hints.contains(code))
-                || subset == "implemented_in_snos" && snos_hints.contains(code)
-            {
-                result.insert(code);
+            let add = match subset {
+                HintStatus::All => true,
+                HintStatus::Implemented => known_to_hint_processor(code),
+                HintStatus::Unimplemented => !known_to_hint_processor(code),
+                HintStatus::ImplementedExternally => known_to_hint_processor(code) && !snos_hints.contains(code),
+                HintStatus::ImplementedLocally => snos_hints.contains(code),
+                _ => unreachable!()
+            };
+            if add {
+                result.push(code);
             }
         }
     }
 
-    println!("{}", serde_json::to_string(&result)?);
-    println!("{} results", result.len());
+    println!("Number of HintStatus::{:?} results: {}", subset, result.len());
 
-    let mut output = File::create("/tmp/out.rs")?;
-    write!(output, "{}", buf.as_str())?;
+    if let Some(filepath) = args.out_file {
+        let buf = match args.out_type.unwrap_or_default() {
+            OutputType::Json => serde_json::to_string(&result)?,
+            OutputType::Rust => generate_hints_as_rust(&result),
+        };
+        let mut output = File::create(filepath)?;
+        write!(output, "{}", buf.as_str())?;
+    }
+
 
     Ok(())
+}
+
+fn generate_hints_as_rust(hints: &Vec<&String>) -> String {
+    let mut buf = String::new();
+    let mut count = 0;
+    for hint in hints {
+        buf.push_str(&format!("pub const UNUSED_HINT_{}", count));
+        buf.push_str(": &str = indoc! {r#\"");
+        buf.push_str(hint.as_str());
+        buf.push_str("\"#};\n\n");
+        count += 1;
+    }
+
+    buf
 }
