9a5ac487c7b29222ba79bd95ab132140aa0ce765
Stephen Shelton <steve@brewcraft.org>
2024-04-09 09:18:27 -0600

    Move hint tool to its own crate

---
+++ bin/hint_tool/src/main.rs
@@ -0,0 +1,94 @@
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{BufReader, Write};
+
+use blockifier::execution::hint_code;
+use cairo_vm::any_box;
+use cairo_vm::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::HintProcessorData;
+use cairo_vm::hint_processor::hint_processor_definition::HintProcessorLogic;
+use cairo_vm::types::exec_scope::ExecutionScopes;
+use cairo_vm::vm::errors::hint_errors::HintError;
+use cairo_vm::vm::vm_core::VirtualMachineBuilder;
+use cairo_vm::vm::vm_memory::memory_segments::MemorySegmentManager;
+use serde::Deserialize;
+use snos::hints::SnosHintProcessor;
+
+#[derive(Deserialize)]
+struct Hint {
+    code: Box<str>,
+}
+
+#[derive(Deserialize)]
+struct Os {
+    hints: HashMap<String, Vec<Hint>>,
+}
+
+fn main() -> std::io::Result<()> {
+    let subset = std::env::args().nth(1).expect(
+        "choose what you need: all, implemented, unimplemented, implemented_externally, implemented_in_snos, orphans",
+    );
+
+    let mut hint_processor = SnosHintProcessor::default();
+    let snos_hints = hint_processor.hints();
+
+    let mut result = HashSet::new();
+
+    let os: Os = serde_json::from_reader(BufReader::new(File::open("build/os_latest.json")?))
+        .expect("Failed to parse os_latest.json");
+    let os_hints = os.hints.into_values().flatten().map(|h| h.code.to_string()).collect::<HashSet<_>>();
+    let syscall_hints = hint_code::SYSCALL_HINTS.into_iter().map(|h| h.to_string()).collect::<HashSet<_>>();
+
+    let segments = MemorySegmentManager::new();
+    let mut vm = VirtualMachineBuilder::default().segments(segments).build();
+
+    let mut buf = String::new();
+    let mut count = 0;
+
+    let mut known_to_hint_processor = |code: &String| {
+        let r = hint_processor.execute_hint_extensive(
+            &mut vm,
+            &mut ExecutionScopes::new(),
+            &any_box!(HintProcessorData::new_default(code.clone(), HashMap::new())),
+            &HashMap::new(),
+        );
+        if let Err(e) = r {
+            if let HintError::UnknownHint(_) = e {
+                println!("\n----- gave unknown hint error: ----- \n{}", code.clone());
+                buf.push_str(&format!("pub const UNUSED_HINT_{}", count));
+                buf.push_str(": &str = indoc! {r#\"");
+                buf.push_str(code.as_str());
+                buf.push_str("\"#};\n\n");
+                count += 1;
+                return false;
+            }
+        }
+        true
+    };
+
+    if subset == "orphans" {
+        for code in snos_hints.iter() {
+            if !os_hints.contains(code) && !syscall_hints.contains(code) {
+                result.insert(code);
+            }
+        }
+    } else {
+        for code in os_hints.union(&syscall_hints).collect::<HashSet<_>>() {
+            if subset == "all"
+                || subset == "implemented" && known_to_hint_processor(code)
+                || subset == "unimplemented" && !known_to_hint_processor(code)
+                || subset == "implemented_externally" && (known_to_hint_processor(code) && !snos_hints.contains(code))
+                || subset == "implemented_in_snos" && snos_hints.contains(code)
+            {
+                result.insert(code);
+            }
+        }
+    }
+
+    println!("{}", serde_json::to_string(&result)?);
+    println!("{} results", result.len());
+
+    let mut output = File::create("/tmp/out.rs")?;
+    write!(output, "{}", buf.as_str())?;
+
+    Ok(())
+}
