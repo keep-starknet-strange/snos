82e2e98dbbd6c223c77a71bc586634d48755aa18
Stephen Shelton <steve@brewcraft.org>
2024-04-16 08:47:36 -0600

    Define BASE as a const

--- src/hints/bls_utils.rs
+++ src/hints/bls_utils.rs
@@ -1,20 +1,23 @@
 use cairo_vm::{vm::errors::hint_errors::HintError, Felt252};
 
+use lazy_static::lazy_static;
 use num_bigint::BigInt;
 use num_integer::Integer;
 use num_traits::Signed;
+
+lazy_static! {
+    static ref BASE: BigInt = BigInt::from(2).pow(86);
+}
 
 /// Takes an integer and returns its canonical representation as:
 ///    d0 + d1 * BASE + d2 * BASE**2.
 /// d2 can be in the range (-2**127, 2**127).
 pub fn split(num: Felt252) -> Result<Vec<Felt252>, HintError> {
 
-    let base: BigInt = BigInt::from(2).pow(86);
-
     let mut a = Vec::with_capacity(3);
     let mut num = num.to_bigint();
     for _ in 0..2 {
-        let (q, residue) = num.div_mod_floor(&base);
+        let (q, residue) = num.div_mod_floor(&BASE);
         num = q;
         a.push(residue);
     }
