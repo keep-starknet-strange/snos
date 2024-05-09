304713a8fabab23f6a97638a333c366470ec41b0
Stephen Shelton <steve@brewcraft.org>
2024-05-09 13:37:51 -0600

    Close, but this will work for non-deprecated class hashing

--- src/starknet/business_logic/fact_state/contract_class_objects.rs
+++ src/starknet/business_logic/fact_state/contract_class_objects.rs
@@ -1,5 +1,8 @@
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
-use starknet_api::deprecated_contract_class::ContractClass as DeprecatedCompiledClass;
+use sha3::Digest as _;
+use starknet_api::{core::ClassHash, deprecated_contract_class::ContractClass as DeprecatedCompiledClass, hash::{poseidon_hash_array, StarkFelt}, state::{ContractClass, EntryPointType}};
+use starknet_crypto::FieldElement;
 
 use crate::storage::storage::{DbObject, Fact, HashFunctionType, Storage};
 
@@ -16,6 +19,58 @@ where
     H: HashFunctionType,
 {
     fn hash(&self) -> Vec<u8> {
-        todo!()
+        calculate_class_hash(&self.contract_definition).0.bytes().into()
     }
+}
+
+lazy_static! {
+    static ref API_VERSION: StarkFelt = StarkFelt::from(
+        FieldElement::from_byte_slice_be(b"CONTRACT_CLASS_V0.1.0")
+            .expect("CONTRACT_CLASS_V0.1.0 is valid StarkFelt."),
+    );
+}
+
+/// Calculate the hash of a contract (v0 or v1). Inspired by the Papyrus implementation.
+pub fn calculate_class_hash(class: &ContractClass) -> ClassHash {
+    let external_entry_points_hash = entry_points_hash(class, &EntryPointType::External);
+    let l1_handler_entry_points_hash = entry_points_hash(class, &EntryPointType::L1Handler);
+    let constructor_entry_points_hash = entry_points_hash(class, &EntryPointType::Constructor);
+    let abi_keccak = sha3::Keccak256::default().chain_update(class.abi.as_bytes()).finalize();
+    let abi_hash = truncated_keccak(abi_keccak.into());
+    let program_hash = poseidon_hash_array(class.sierra_program.as_slice());
+
+    let class_hash = poseidon_hash_array(&[
+        *API_VERSION,
+        external_entry_points_hash.0,
+        l1_handler_entry_points_hash.0,
+        constructor_entry_points_hash.0,
+        abi_hash,
+        program_hash.0,
+    ]);
+    // TODO: Modify ClassHash Be be PoseidonHash instead of StarkFelt.
+    ClassHash(class_hash.0)
+}
+
+fn entry_points_hash(class: &ContractClass, entry_point_type: &EntryPointType) -> starknet_api::hash::PoseidonHash {
+    poseidon_hash_array(
+        class
+            .entry_point_by_type
+            .get(entry_point_type)
+            .unwrap_or(&vec![])
+            .iter()
+            .flat_map(|ep| [ep.selector.0, usize_into_felt(ep.function_idx.0)])
+            .collect::<Vec<_>>()
+            .as_slice(),
+    )
+}
+
+// Python code masks with (2**250 - 1) which starts 0x03 and is followed by 31 0xff in be.
+// Truncation is needed not to overflow the field element.
+fn truncated_keccak(mut plain: [u8; 32]) -> StarkFelt {
+    plain[0] &= 0x03;
+    StarkFelt::new(plain).unwrap()
+}
+
+pub(crate) fn usize_into_felt(u: usize) -> StarkFelt {
+    u128::try_from(u).expect("Expect at most 128 bits").into()
 }
--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -2,3 +2,6 @@ mod common;
 mod os;
 mod pie;
 mod sharp;
+
+#[macro_use]
+extern crate lazy_static;
