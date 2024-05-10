570c427db7aabe82aaa7642d7a3a1d5dd74fb9d0
Stephen Shelton <steve@brewcraft.org>
2024-05-10 09:03:41 -0600

    Wrong but hopefully deterministic cairo0 class hashing

--- src/starknet/business_logic/fact_state/contract_class_objects.rs
+++ src/starknet/business_logic/fact_state/contract_class_objects.rs
@@ -1,7 +1,7 @@
 use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use sha3::Digest as _;
-use starknet_api::{core::ClassHash, deprecated_contract_class::ContractClass as DeprecatedCompiledClass, hash::{poseidon_hash_array, StarkFelt}, state::{ContractClass, EntryPointType}};
+use starknet_api::{core::ClassHash, deprecated_contract_class::ContractClass as DeprecatedCompiledClass, hash::{pedersen_hash_array, poseidon_hash_array, StarkFelt}, deprecated_contract_class::EntryPointType};
 use starknet_crypto::FieldElement;
 
 use crate::storage::storage::{DbObject, Fact, HashFunctionType, Storage};
@@ -19,11 +19,12 @@ where
     H: HashFunctionType,
 {
     fn hash(&self) -> Vec<u8> {
-        calculate_class_hash(&self.contract_definition).0.bytes().into()
+        calculate_deprecated_class_hash(&self.contract_definition).0.bytes().into()
     }
 }
 
 lazy_static! {
+    // TODO: is this the same for deprecated class hash?
     static ref API_VERSION: StarkFelt = StarkFelt::from(
         FieldElement::from_byte_slice_be(b"CONTRACT_CLASS_V0.1.0")
             .expect("CONTRACT_CLASS_V0.1.0 is valid StarkFelt."),
@@ -31,34 +32,42 @@ lazy_static! {
 }
 
 /// Calculate the hash of a contract (v0 or v1). Inspired by the Papyrus implementation.
-pub fn calculate_class_hash(class: &ContractClass) -> ClassHash {
-    let external_entry_points_hash = entry_points_hash(class, &EntryPointType::External);
-    let l1_handler_entry_points_hash = entry_points_hash(class, &EntryPointType::L1Handler);
-    let constructor_entry_points_hash = entry_points_hash(class, &EntryPointType::Constructor);
-    let abi_keccak = sha3::Keccak256::default().chain_update(class.abi.as_bytes()).finalize();
-    let abi_hash = truncated_keccak(abi_keccak.into());
-    let program_hash = poseidon_hash_array(class.sierra_program.as_slice());
+/// TODO: this needs to be rewritten, there are many details missing
+pub fn calculate_deprecated_class_hash(class: &DeprecatedCompiledClass) -> ClassHash {
+    let mut hash_chain = Vec::new();
+    hash_chain.push(*API_VERSION);
 
-    let class_hash = poseidon_hash_array(&[
-        *API_VERSION,
-        external_entry_points_hash.0,
-        l1_handler_entry_points_hash.0,
-        constructor_entry_points_hash.0,
-        abi_hash,
-        program_hash.0,
-    ]);
+    hash_chain.push(deprecated_entry_points_hash(class, &EntryPointType::External).0);
+    hash_chain.push(deprecated_entry_points_hash(class, &EntryPointType::L1Handler).0);
+    hash_chain.push(deprecated_entry_points_hash(class, &EntryPointType::Constructor).0);
+
+    // TODO: review -- what should be done when no ABI is given?
+    if let Some(abi) = &class.abi {
+        // TODO: wrong
+        let abi_serialized = serde_json::to_string(&abi).expect("Serialized Cairo0 program should be deserializable");
+
+        let abi_keccak = sha3::Keccak256::default().chain_update(abi_serialized.as_bytes()).finalize();
+        let as_bytes: [u8; 32] = abi_keccak[..32].try_into().unwrap();
+        hash_chain.push(StarkFelt::new(as_bytes).unwrap()); // TODO: should this be truncated to fit with in prime?
+    }
+
+    // TODO: definitely wrong...
+    let bytecode_hash = pedersen_hash_array(&serde_json::to_string(&class.program.data).unwrap().as_bytes().into_iter().map(|b| StarkFelt::from(*b)).collect::<Vec<StarkFelt>>()[..]);
+    hash_chain.push(bytecode_hash);
+
+    let class_hash = pedersen_hash_array(&hash_chain[..]);
     // TODO: Modify ClassHash Be be PoseidonHash instead of StarkFelt.
-    ClassHash(class_hash.0)
+    ClassHash(class_hash.into())
 }
 
-fn entry_points_hash(class: &ContractClass, entry_point_type: &EntryPointType) -> starknet_api::hash::PoseidonHash {
+fn deprecated_entry_points_hash(class: &DeprecatedCompiledClass, entry_point_type: &EntryPointType) -> starknet_api::hash::PoseidonHash {
     poseidon_hash_array(
         class
-            .entry_point_by_type
+            .entry_points_by_type
             .get(entry_point_type)
             .unwrap_or(&vec![])
             .iter()
-            .flat_map(|ep| [ep.selector.0, usize_into_felt(ep.function_idx.0)])
+            .flat_map(|ep| [ep.selector.0, usize_into_felt(ep.offset.0)])
             .collect::<Vec<_>>()
             .as_slice(),
     )
