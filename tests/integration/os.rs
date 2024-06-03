bf5deec1779ec07b54004dbed4b1794f717f4775
Stephen Shelton <steve@brewcraft.org>
2024-06-03 17:11:37 -0600

    Progress on cairo1 declare

--- tests/integration/os.rs
+++ tests/integration/os.rs
@@ -5,13 +5,18 @@ use blockifier::transaction::test_utils::max_fee;
 use blockifier::{declare_tx_args, invoke_tx_args};
 use rstest::rstest;
 use snos::config::STORED_BLOCK_HASH_BUFFER;
+use snos::crypto::poseidon::PoseidonHash;
+use snos::starknet::business_logic::utils::write_class_facts;
+use snos::storage::dict_storage::DictStorage;
+use snos::storage::storage::FactFetchingContext;
 use snos::storage::storage_utils::compiled_contract_class_cl2vm;
+use starknet_api::core::CompiledClassHash;
 use starknet_api::hash::StarkFelt;
 use starknet_api::stark_felt;
 use starknet_api::transaction::{Fee, TransactionVersion};
 
 use crate::common::block_context;
-use crate::common::state::{initial_state_cairo0, initial_state_cairo1, initial_state_syscalls, StarknetTestState};
+use crate::common::state::{initial_state_cairo0, initial_state_cairo1, initial_state_syscalls, load_cairo1_contract, StarknetTestState};
 use crate::common::transaction_utils::execute_txs_and_run_os;
 
 #[rstest]
@@ -136,11 +141,23 @@ async fn declare_and_deploy_account_cairo1_account(
 
     let tx_version = TransactionVersion::TWO;
     let mut nonce_manager = NonceManager::default();
+    
+    let account_contract = initial_state.cairo1_contracts.get("account_with_dummy_validate").unwrap();
 
-    let sender_address = initial_state.cairo1_contracts.get("account_with_dummy_validate").unwrap().address;
+    // we want to declare a fresh (never-before-declared) contract, so we don't want to reuse
+    // anything from the test fixtures, and we need to do it "by hand"
+    let (_, sierra_class, casm_class) = load_cairo1_contract("empty_contract");
+    let mut ffc: FactFetchingContext<DictStorage, PoseidonHash> = FactFetchingContext::new(DictStorage::default());
+    let (contract_class_hash, compiled_class_hash) =
+        write_class_facts(sierra_class.clone(), casm_class.clone(), &mut ffc)
+            .await
+            .unwrap();
 
-    let casm_class = &initial_state.cairo1_contracts.get("account_with_dummy_validate").unwrap().casm_class;
-    let contract_class = compiled_contract_class_cl2vm(casm_class).unwrap();
+    let sender_address = account_contract.address;
+
+    let contract_class = compiled_contract_class_cl2vm(&casm_class).unwrap();
+    let class_hash = starknet_api::core::ClassHash::try_from(contract_class_hash).unwrap();
+    let compiled_class_hash = CompiledClassHash::try_from(compiled_class_hash).unwrap();
 
     let declare_tx = blockifier::test_utils::declare::declare_tx(
         declare_tx_args! {
@@ -148,6 +165,8 @@ async fn declare_and_deploy_account_cairo1_account(
             sender_address,
             version: tx_version,
             nonce: nonce_manager.next(sender_address),
+            class_hash: class_hash.into(),
+            compiled_class_hash,
         },
         contract_class,
     );
