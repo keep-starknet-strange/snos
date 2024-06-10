578f0fbfa63e18fc126071df5e676f8652475c24
Olivier Desenfans <desenfans.olivier@gmail.com>
2024-06-10 21:34:07 +0200

    Feature: support Cairo 0 `get_tx_info()` syscall
    Added support for `get_tx_info()` + a test.
    
    Note that the test needed a new function in `test_contract.cairo` as the
    test for this syscall was impractical for integration testing.

--- src/cairo_types/structs.rs
+++ src/cairo_types/structs.rs
@@ -1,15 +1,15 @@
 use cairo_type_derive::FieldOffsetGetters;
+use cairo_vm::types::relocatable::Relocatable;
 use cairo_vm::Felt252;
 
-#[allow(unused)]
 #[derive(FieldOffsetGetters)]
 pub struct ExecutionContext {
     pub entry_point_type: Felt252,
     pub class_hash: Felt252,
     pub calldata_size: Felt252,
-    pub calldata: Felt252,
-    pub execution_info: Felt252,
-    pub deprecated_tx_info: Felt252,
+    pub calldata: Relocatable,
+    pub execution_info: Relocatable,
+    pub deprecated_tx_info: Relocatable,
 }
 
 #[allow(unused)]
