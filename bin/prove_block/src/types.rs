1a6054d224c63405b95ddf2e758daa4d989aaefc
Olivier Desenfans <desenfans.olivier@gmail.com>
2024-06-19 16:34:14 +0200

    WIP: block_context ok

---
+++ bin/prove_block/src/types.rs
@@ -0,0 +1,62 @@
+//! Contains simplified types for parsing JSON-RPC responses.
+//!
+//! In order not to depend on pathfinder_lib these types "duplicate" similar
+//! functionality already found in pathfinder. However, these types are
+//! simplified and are missing fields that are irrelevant for load tests.
+use pathfinder_common::GasPrice;
+use pathfinder_crypto::Felt;
+use serde_with::serde_as;
+
+#[serde_as]
+#[derive(Clone, Debug, serde::Deserialize, PartialEq, Eq)]
+pub struct ResourcePrice {
+    #[serde_as(as = "pathfinder_serde::GasPriceAsHexStr")]
+    pub price_in_wei: GasPrice,
+    #[serde_as(as = "pathfinder_serde::GasPriceAsHexStr")]
+    pub price_in_fri: GasPrice,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, PartialEq, Eq)]
+pub struct Block {
+    pub block_hash: Felt,
+    pub parent_hash: Felt,
+    pub block_number: u64,
+    pub new_root: Felt,
+    pub timestamp: u64,
+    pub sequencer_address: Felt,
+    pub l1_gas_price: ResourcePrice,
+    pub l1_data_gas_price: ResourcePrice,
+    // pub transactions: Vec<Felt>,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, PartialEq, Eq)]
+pub struct Transaction {
+    pub r#type: String,
+    pub transaction_hash: Felt,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, PartialEq, Eq)]
+pub struct TransactionReceipt {
+    pub r#type: String,
+    pub transaction_hash: Felt,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, PartialEq, Eq)]
+pub struct StateUpdate {
+    pub block_hash: Felt,
+    pub new_root: Felt,
+    pub old_root: Felt,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, PartialEq, Eq)]
+pub struct ContractClass {
+    pub abi: serde_json::Value,
+    pub program: String,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, PartialEq, Eq)]
+pub struct FeeEstimate {
+    pub gas_consumed: String,
+    pub gas_price: String,
+    pub overall_fee: String,
+}
