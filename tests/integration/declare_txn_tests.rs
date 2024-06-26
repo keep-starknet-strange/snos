0e3a9885c8429a52a4cf2ae1e6c4721aaf4aedbf
whichqua <170323628+whichqua@users.noreply.github.com>
2024-06-26 18:35:29 +0300

    lint: cargo fmt

--- tests/integration/declare_txn_tests.rs
+++ tests/integration/declare_txn_tests.rs
@@ -83,7 +83,6 @@ async fn declare_v3_cairo1_account(
     .expect("OS run failed");
 }
 
-
 #[rstest]
 // We need to use the multi_thread runtime to use task::block_in_place for sync -> async calls.
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
