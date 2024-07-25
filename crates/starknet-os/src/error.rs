3d7afdd992b91eafd3ccdbb9f4cbf4b7efa21336
Olivier Desenfans <olivier@moonsonglabs.com>
2024-07-25 13:34:16 +0200

    Internal: remove dead code from the error module (#275)

--- crates/starknet-os/src/error.rs
+++ crates/starknet-os/src/error.rs
@@ -24,17 +24,3 @@ pub enum SnOsError {
     #[error("SnOs Deprecated Syscall Error: {0}")]
     InvalidDeprecatedSyscallSelector(Felt252),
 }
-
-#[derive(thiserror::Error, Clone, Debug)]
-pub enum CommitmentInfoError {
-    #[error("Inconsistent tree heights : {0} {1}.")]
-    InconsistentTreeHeights(usize, usize),
-    #[error("Inconsistent tree roots, actual : {0} , expected : {1}.")]
-    InconsistentTreeRoots(Felt252, Felt252),
-}
-
-#[derive(thiserror::Error, Clone, Debug)]
-pub enum FactTreeError {
-    #[error("Unexpected result on single leaf index : {0}")]
-    UnexpectedResult(Felt252),
-}
