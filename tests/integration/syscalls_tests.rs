8de373aa36133d08fe46cd99cd1d6a88ea842f67
whichqua <geoffrey@moonsonglabs.com>
2024-07-30 18:50:27 +0300

    fix: assert secp tests are failing due to missing hint

--- tests/integration/syscalls_tests.rs
+++ tests/integration/syscalls_tests.rs
@@ -110,7 +110,9 @@ async fn test_syscall_replace_class_cairo1(
 #[rstest(
     curve_type => ["test_secp256k1", "test_secp256r1"]
 )]
-// We need to use the multi_thread runtime to use task::block_in_place for sync -> async calls.
+// TODO: This is a workaround as the secp hints are being worked on
+#[should_panic(expected = "UnknownHint(\"memory[ap] = to_felt_or_relocatable(ids.response.ec_point.address_ if \
+                           ids.not_on_curve == 0 else segments.add())\")")]
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn test_syscall_test_secp_cairo1(
     #[future] initial_state_syscalls: StarknetTestState,
