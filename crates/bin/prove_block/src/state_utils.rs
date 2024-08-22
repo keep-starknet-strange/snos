3e22dbfafb6e23c4456fcdb4e47e53a0eb866e21
Herman Obst Demaestri <hodemaestri@gmail.com>
2024-08-22 14:28:41 -0700

    sanity check before removing code from main

--- crates/bin/prove_block/src/state_utils.rs
+++ crates/bin/prove_block/src/state_utils.rs
@@ -1,8 +1,10 @@
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
+use std::hash::Hash;
 
 use cairo_vm::Felt252;
-use starknet::core::types::BlockId;
+use pathfinder_crypto::Felt;
+use starknet::core::types::{BlockId, MaybePendingStateUpdate};
 use starknet::providers::jsonrpc::HttpTransport;
 use starknet::providers::{JsonRpcClient, Provider};
 use starknet_os::crypto::pedersen::PedersenHash;
@@ -17,6 +19,14 @@ use starknet_os::starkware_utils::commitment_tree::binary_fact_tree::BinaryFactT
 use starknet_os::storage::storage::FactFetchingContext;
 
 use crate::rpc_utils::{CachedRpcStorage, RpcStorage};
+
+struct ProcessedStateUpdate {
+    // TODO: Use more descriptive types
+    address_to_class_hash: HashMap<Felt, Felt>,
+    address_to_nonce: HashMap<Felt, Felt>,
+    class_hash_to_compiled_class_hash: HashMap<Felt, Felt>,
+    storage_updates: HashMap<Felt, HashMap<Felt, Felt>>
+}
 
 // build state representing the end of the previous block on which the current
 // block can be built.
@@ -46,12 +56,12 @@ pub(crate) async fn build_initial_state(
 
     let mut facts = None;
     let mut ffc = shared_state.ffc;
-    let mut current_contract_states: HashMap<TreeIndex, ContractState> =
+    let mut empty_contract_states: HashMap<TreeIndex, ContractState> =
         shared_state.contract_states.get_leaves(&mut ffc, &accessed_addresses, &mut facts).await?;
 
     // Update contract storage roots with cached changes.
     let empty_updates = HashMap::new();
-    let mut updated_contract_states = HashMap::new();
+    let mut updated_contract_states: HashMap<num_bigint::BigUint, ContractState> = HashMap::new();
     for address in accessed_addresses_felts {
         // unwrap() is safe as an entry is guaranteed to be present with `get_leaves()`.
         let tree_index = address.to_biguint();
@@ -63,7 +73,7 @@ pub(crate) async fn build_initial_state(
             class_hash = if let Ok(class_hash) = resp { Some(class_hash) } else { Some(Felt252::ZERO) };
         }
         let updated_contract_state =
-            current_contract_states.remove(&tree_index).unwrap().update(&mut ffc, updates, nonce, class_hash).await?;
+            empty_contract_states.remove(&tree_index).unwrap().update(&mut ffc, updates, nonce, class_hash).await?;
 
         updated_contract_states.insert(tree_index, updated_contract_state);
     }
@@ -107,4 +117,36 @@ pub(crate) async fn build_initial_state(
             contract_addresses: Default::default(),
         },
     ))
+}
+
+async fn update_empty_state_with_block_incoming_changes(empty_contract_states: &HashMap<TreeIndex, ContractState>) -> Result<HashMap<TreeIndex, ContractState>, Box<dyn Error>> {
+        // Update contract storage roots with cached changes.
+        let empty_updates = HashMap::new();
+        let mut updated_contract_states: HashMap<num_bigint::BigUint, ContractState> = HashMap::new();
+        for address in accessed_addresses_felts {
+            // unwrap() is safe as an entry is guaranteed to be present with `get_leaves()`.
+            let tree_index = address.to_biguint();
+            let updates = storage_updates.get(address).unwrap_or(&empty_updates);
+            let nonce = address_to_nonce.get(address).cloned();
+            let mut class_hash = address_to_class_hash.get(address).cloned();
+            if class_hash.is_none() {
+                let resp = provider.get_class_hash_at(BlockId::Number(block_number), address).await;
+                class_hash = if let Ok(class_hash) = resp { Some(class_hash) } else { Some(Felt252::ZERO) };
+            }
+            let updated_contract_state =
+                empty_contract_states.remove(&tree_index).unwrap().update(&mut ffc, updates, nonce, class_hash).await?;
+    
+            updated_contract_states.insert(tree_index, updated_contract_state);
+        }
+        Ok(updated_contract_states)
+}
+
+async fn get_processed_state_update(provider: &JsonRpcClient<HttpTransport>, block_id: BlockId) -> ProcessedStateUpdate {
+    let state_update = match provider.get_state_update(block_id).await.expect("Failed to get state update") {
+        MaybePendingStateUpdate::Update(update) => update,
+        MaybePendingStateUpdate::PendingUpdate(_) => {
+            panic!("Block is still pending!")
+        }
+    };
+    todo!()
 }
