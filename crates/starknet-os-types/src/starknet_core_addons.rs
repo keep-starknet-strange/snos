d820c4ef14a41f4abfca8da408903241b6417135
Olivier Desenfans <olivier@moonsonglabs.com>
2024-08-22 22:15:37 +0200

    Fix: decompress starknet-core legacy class (#313)
    Problem: the conversion from starknet-core `CompressedLegacyContractClass`
    in `GenericDeprecatedContractClass` is untested and broken. This class
    cannot be used interchangeably with other deprecated contract classes
    because the `program` field is deserialized as a gzip bytes vector.
    
    Solution: add conversion functions to decompress the program field and
    generate a `LegacyContractClass` from it before storing it in the
    generic class.

---
+++ crates/starknet-os-types/src/starknet_core_addons.rs
@@ -0,0 +1,142 @@
+//! Utility functions missing from starknet-rs.
+use std::io::Read;
+
+use flate2::read::GzDecoder;
+use starknet_core::types::contract::legacy::{
+    LegacyContractClass, LegacyEntrypointOffset, RawLegacyAbiEntry, RawLegacyEntryPoint, RawLegacyEntryPoints,
+    RawLegacyEvent, RawLegacyFunction, RawLegacyL1Handler, RawLegacyMember, RawLegacyStruct,
+};
+use starknet_core::types::{
+    CompressedLegacyContractClass, LegacyContractAbiEntry, LegacyContractEntryPoint, LegacyEntryPointsByType,
+    LegacyFunctionAbiEntry, LegacyFunctionAbiType, LegacyStructMember,
+};
+
+fn raw_abi_entry_from_legacy_function_abi_entry(entry: LegacyFunctionAbiEntry) -> RawLegacyAbiEntry {
+    match entry.r#type {
+        LegacyFunctionAbiType::Function | LegacyFunctionAbiType::Constructor => {
+            RawLegacyAbiEntry::Function(RawLegacyFunction {
+                inputs: entry.inputs,
+                name: entry.name,
+                outputs: entry.outputs,
+                state_mutability: entry.state_mutability,
+            })
+        }
+        LegacyFunctionAbiType::L1Handler => RawLegacyAbiEntry::L1Handler(RawLegacyL1Handler {
+            inputs: entry.inputs,
+            name: entry.name,
+            outputs: entry.outputs,
+        }),
+    }
+}
+
+fn raw_legacy_member_from_legacy_struct_member(member: LegacyStructMember) -> RawLegacyMember {
+    RawLegacyMember { name: member.name, offset: member.offset, r#type: member.r#type }
+}
+
+/// Implementation of From<LegacyContractAbiEntry> for RawLegacyAbiEntry,
+/// as it is missing from starknet-rs and we need it to interpret compressed legacy contracts.
+fn raw_legacy_abi_entry_from_legacy_contract_abi_entry(
+    legacy_contract_abi_entry: LegacyContractAbiEntry,
+) -> RawLegacyAbiEntry {
+    match legacy_contract_abi_entry {
+        LegacyContractAbiEntry::Function(entry) => raw_abi_entry_from_legacy_function_abi_entry(entry),
+        LegacyContractAbiEntry::Struct(entry) => RawLegacyAbiEntry::Struct(RawLegacyStruct {
+            members: entry.members.into_iter().map(raw_legacy_member_from_legacy_struct_member).collect(),
+            name: entry.name,
+            size: entry.size,
+        }),
+        LegacyContractAbiEntry::Event(entry) => {
+            RawLegacyAbiEntry::Event(RawLegacyEvent { data: entry.data, keys: entry.keys, name: entry.name })
+        }
+    }
+}
+
+fn raw_legacy_entrypoint_from_legacy_entrypoint(legacy_entry_point: LegacyContractEntryPoint) -> RawLegacyEntryPoint {
+    RawLegacyEntryPoint {
+        offset: LegacyEntrypointOffset::U64AsHex(legacy_entry_point.offset),
+        selector: legacy_entry_point.selector,
+    }
+}
+
+/// Implementation of From<LegacyEntryPointsByType> for RawLegacyEntryPoints,
+/// as it is missing from starknet-rs and we need it to interpret compressed legacy contracts.
+fn raw_legacy_entrypoints_from_legacy_entrypoints(
+    legacy_entry_points_by_type: LegacyEntryPointsByType,
+) -> RawLegacyEntryPoints {
+    RawLegacyEntryPoints {
+        constructor: legacy_entry_points_by_type
+            .constructor
+            .into_iter()
+            .map(raw_legacy_entrypoint_from_legacy_entrypoint)
+            .collect(),
+        external: legacy_entry_points_by_type
+            .external
+            .into_iter()
+            .map(raw_legacy_entrypoint_from_legacy_entrypoint)
+            .collect(),
+        l1_handler: legacy_entry_points_by_type
+            .l1_handler
+            .into_iter()
+            .map(raw_legacy_entrypoint_from_legacy_entrypoint)
+            .collect(),
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum LegacyContractDecompressionError {
+    #[error("Failed to deserialize the program as JSON: {0}")]
+    Serde(#[from] serde_json::Error),
+
+    #[error("Failed to decompress the program: {0}")]
+    Decompression(#[from] std::io::Error),
+}
+
+/// Decompresses a compressed legacy contract class.
+/// Compressed classes store the `program` field as a gzipped vector. This function
+/// decompresses the field and performs additional type conversions for the `abi` and
+/// `entry_points_by_type` fields.
+pub(crate) fn decompress_starknet_core_contract_class(
+    compressed_legacy_class: CompressedLegacyContractClass,
+) -> Result<LegacyContractClass, LegacyContractDecompressionError> {
+    let mut program_str = String::new();
+    let mut decoder = GzDecoder::new(compressed_legacy_class.program.as_slice());
+    decoder.read_to_string(&mut program_str)?;
+
+    let program: starknet_core::types::contract::legacy::LegacyProgram = serde_json::from_str(&program_str)?;
+    let abi = compressed_legacy_class
+        .abi
+        .unwrap_or_default()
+        .into_iter()
+        .map(raw_legacy_abi_entry_from_legacy_contract_abi_entry)
+        .collect();
+
+    Ok(LegacyContractClass {
+        abi,
+        entry_points_by_type: raw_legacy_entrypoints_from_legacy_entrypoints(
+            compressed_legacy_class.entry_points_by_type,
+        ),
+        program,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use starknet_core::types::contract::legacy::LegacyContractClass;
+
+    use crate::starknet_core_addons::decompress_starknet_core_contract_class;
+
+    const DEPRECATED_CLASS: &[u8] = include_bytes!(
+        "../../../tests/integration/contracts/blockifier_contracts/feature_contracts/cairo0/compiled/\
+         test_contract_compiled.json"
+    );
+
+    #[test]
+    /// Test that compressing then decompressing a legacy class works.
+    /// `LegacyContractClass` does not implement `PartialEq` so we'll content ourselves
+    /// by checking that the test does not fail.
+    fn test_decompress_legacy_class() {
+        let legacy_class: LegacyContractClass = serde_json::from_slice(DEPRECATED_CLASS).unwrap();
+        let compressed_legacy_class = legacy_class.compress().unwrap();
+        let _decompressed_legacy_class = decompress_starknet_core_contract_class(compressed_legacy_class).unwrap();
+    }
+}
