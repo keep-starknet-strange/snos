77a998ac76b6b081f1da3055108b68ba6a503c1a
Olivier Desenfans <desenfans.olivier@gmail.com>
2024-08-29 10:57:14 +0200

    doc chain_id

--- crates/starknet-os-types/src/chain_id.rs
+++ crates/starknet-os-types/src/chain_id.rs
@@ -6,7 +6,10 @@ pub fn chain_id_to_felt(chain_id: &ChainId) -> Felt {
     Felt::from_bytes_be_slice(chain_id.to_string().as_bytes())
 }
 
+/// Builds a ChainId from a felt.
+/// This function reads the felt as ASCII bytes. Leading zeroes are skipped.
 pub fn chain_id_from_felt(felt: Felt) -> ChainId {
+    // Skip leading zeroes
     let chain_id_bytes: Vec<_> = felt.to_bytes_be().into_iter().skip_while(|byte| *byte == 0u8).collect();
     let chain_id_str = String::from_utf8_lossy(&chain_id_bytes);
     ChainId::from(chain_id_str.into_owned())
