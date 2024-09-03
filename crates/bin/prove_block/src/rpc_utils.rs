837551882e26b883ff89bef069aa6da9fdb68c66
Olivier Desenfans <olivier@moonsonglabs.com>
2024-09-04 01:07:47 +0200

    Fix: support writes to zero (#341)
    Problem: we encounter an "Edge bottom not found in preimage" error when
    running some blocks. This problem was diagnosed as writes to zero where
    the last node of the storage proof was an edge node. A write to zero to
    a specific address will clear the path to that leaf node, so the storage
    proof for this key will always be incomplete. It turns out that the OS
    will still look for the bottom node in the preimage to verify the node
    hash.
    
    Solution: if we encounter such a case, we now fetch an additional key
    that is designed to follow the edge path. This way, we get the bottom
    node in the preimage and the OS can compute the hash it wants to
    compute.

--- crates/bin/prove_block/src/rpc_utils.rs
+++ crates/bin/prove_block/src/rpc_utils.rs
@@ -7,10 +7,12 @@ use serde::Deserialize;
 use serde_json::json;
 use starknet::core::types::BlockWithTxs;
 use starknet_api::core::{ContractAddress, PatriciaKey};
+use starknet_api::state::StorageKey;
 use starknet_api::{contract_address, felt, patricia_key};
+use starknet_os::config::DEFAULT_STORAGE_TREE_HEIGHT;
 use starknet_os::crypto::pedersen::PedersenHash;
 use starknet_os::crypto::poseidon::PoseidonHash;
-use starknet_os::starkware_utils::commitment_tree::base_types::{Length, NodePath};
+use starknet_os::starkware_utils::commitment_tree::base_types::{Height, Length, NodePath};
 use starknet_os::starkware_utils::commitment_tree::patricia_tree::nodes::{BinaryNodeFact, EdgeNodeFact};
 use starknet_os::storage::dict_storage::DictStorage;
 use starknet_os::storage::storage::{Fact, HashFunctionType};
@@ -94,12 +96,16 @@ pub(crate) struct ContractData {
 
 impl ContractData {
     /// Verifies that each contract state proof is valid.
-    pub(crate) fn verify(&self, storage_keys: &[Felt252]) -> Result<(), String> {
+    pub(crate) fn verify(&self, storage_keys: &[Felt252]) -> Result<(), Vec<ProofVerificationError>> {
+        let mut errors = vec![];
+
         for (index, storage_key) in storage_keys.iter().enumerate() {
-            verify_proof::<PedersenHash>(*storage_key, self.root, &self.storage_proofs[index])?;
+            if let Err(e) = verify_proof::<PedersenHash>(*storage_key, self.root, &self.storage_proofs[index]) {
+                errors.push(e);
+            }
         }
 
-        Ok(())
+        if errors.is_empty() { Ok(()) } else { Err(errors) }
     }
 }
 
@@ -127,6 +133,100 @@ pub(crate) async fn pathfinder_get_proof(
     .await
 }
 
+/// Fetches the state + storage proof for a single contract for all the specified keys.
+/// This function handles the chunking of requests imposed by the RPC API and merges
+/// the proofs returned from multiple calls into one.
+async fn fetch_storage_proof_for_contract(
+    client: &reqwest::Client,
+    rpc_provider: &str,
+    contract_address: Felt,
+    keys: &[Felt],
+    block_number: u64,
+) -> Result<PathfinderProof, reqwest::Error> {
+    let storage_proof = if keys.is_empty() {
+        pathfinder_get_proof(client, rpc_provider, block_number, contract_address, &[]).await?
+    } else {
+        // The endpoint is limited to 100 keys at most per call
+        const MAX_KEYS: usize = 100;
+        let mut chunked_storage_proofs = Vec::new();
+        for keys_chunk in keys.chunks(MAX_KEYS) {
+            chunked_storage_proofs
+                .push(pathfinder_get_proof(client, rpc_provider, block_number, contract_address, keys_chunk).await?);
+        }
+        merge_storage_proofs(chunked_storage_proofs)
+    };
+
+    Ok(storage_proof)
+}
+
+/// Fetches the storage proof for the specified contract and storage keys.
+/// This function can fetch additional keys if required to fill gaps in the storage trie
+/// that must be filled to get the OS to function. See `get_key_following_edge` for more details.
+async fn get_storage_proof_for_contract<KeyIter: Iterator<Item = StorageKey>>(
+    client: &reqwest::Client,
+    rpc_provider: &str,
+    contract_address: ContractAddress,
+    storage_keys: KeyIter,
+    block_number: u64,
+) -> Result<PathfinderProof, reqwest::Error> {
+    let contract_address_felt = *contract_address.key();
+    let keys: Vec<_> = storage_keys.map(|storage_key| *storage_key.key()).collect();
+
+    let mut storage_proof =
+        fetch_storage_proof_for_contract(client, rpc_provider, contract_address_felt, &keys, block_number).await?;
+
+    let contract_data = storage_proof.contract_data.as_ref().expect("Storage proof should have contract_data");
+    let additional_keys = verify_storage_proof(contract_data, &keys);
+
+    // Fetch additional proofs required to fill gaps in the storage trie that could make
+    // the OS crash otherwise.
+    if !additional_keys.is_empty() {
+        let additional_proof = fetch_storage_proof_for_contract(
+            client,
+            rpc_provider,
+            contract_address_felt,
+            &additional_keys,
+            block_number,
+        )
+        .await?;
+
+        storage_proof = merge_storage_proofs(vec![storage_proof, additional_proof]);
+    }
+
+    Ok(storage_proof)
+}
+
+/// Verify the storage proofs and handle errors.
+/// Returns a list of additional keys to fetch to fill gaps in the tree that will make the OS
+/// crash otherwise.
+/// This function will panic if the proof contains an invalid node hash (i.e. the hash of a child
+/// node does not match the one specified in the parent).
+fn verify_storage_proof(contract_data: &ContractData, keys: &[Felt]) -> Vec<Felt> {
+    let mut additional_keys = vec![];
+    if let Err(errors) = contract_data.verify(keys) {
+        for error in errors {
+            match error {
+                ProofVerificationError::KeyNotInProof { key, height, proof } => {
+                    if let Some(TrieNode::Edge { child: _, path }) = proof.last() {
+                        let modified_key = get_key_following_edge(key, height, path);
+                        log::debug!(
+                            "Fetching modified key {} for key {}",
+                            modified_key.to_hex_string(),
+                            key.to_hex_string()
+                        );
+                        additional_keys.push(modified_key);
+                    }
+                }
+                ProofVerificationError::InvalidChildNodeHash { .. } => {
+                    panic!("Proof verification failed: {}", error);
+                }
+            }
+        }
+    }
+
+    additional_keys
+}
+
 pub(crate) async fn get_storage_proofs(
     client: &reqwest::Client,
     rpc_provider: &str,
@@ -150,40 +250,36 @@ pub(crate) async fn get_storage_proofs(
 
     for (contract_address, storage_keys) in accessed_keys_by_address {
         let contract_address_felt = *contract_address.key();
-
-        let keys: Vec<_> = storage_keys.iter().map(|storage_key| *storage_key.key()).collect();
-
-        let storage_proof = if keys.is_empty() {
-            pathfinder_get_proof(client, rpc_provider, block_number, contract_address_felt, &[]).await?
-        } else {
-            // The endpoint is limited to 100 keys at most per call
-            const MAX_KEYS: usize = 100;
-            let mut chunked_storage_proofs = Vec::new();
-            for keys_chunk in keys.chunks(MAX_KEYS) {
-                chunked_storage_proofs.push(
-                    pathfinder_get_proof(client, rpc_provider, block_number, contract_address_felt, keys_chunk).await?,
-                );
-            }
-            merge_chunked_storage_proofs(chunked_storage_proofs)
-        };
-
-        if let Err(e) =
-            storage_proof.contract_data.as_ref().expect("Storage proof should have contract_data").verify(&keys)
-        {
-            log::warn!(
-                "Contract {:x} failed storage proof validaiton, may cause problems later in OS, err: {}",
-                contract_address_felt,
-                e
-            );
-        }
-
+        let storage_proof = get_storage_proof_for_contract(
+            client,
+            rpc_provider,
+            contract_address,
+            storage_keys.into_iter(),
+            block_number,
+        )
+        .await?;
         storage_proofs.insert(contract_address_felt, storage_proof);
     }
 
     Ok(storage_proofs)
 }
 
-fn merge_chunked_storage_proofs(proofs: Vec<PathfinderProof>) -> PathfinderProof {
+/// Returns a modified key that follows the specified edge path.
+/// This function is used to work around an issue where the OS fails if it encounters a
+/// write to 0 and the last node in the storage proof is an edge node of length 1.
+/// In this situation the OS will still look up the node in the preimage and will fail
+/// on a "Edge bottom not found in preimage" error.
+/// To resolve this, we fetch the storage proof for a node that follows this edge in order
+/// to get the bottom node in the preimage and resolve the issue.
+fn get_key_following_edge(key: Felt, height: Height, edge_path: &EdgePath) -> Felt {
+    assert!(height.0 < DEFAULT_STORAGE_TREE_HEIGHT);
+    let mask = edge_path.value.to_bigint() << (DEFAULT_STORAGE_TREE_HEIGHT - height.0);
+    let new_key = (key.to_bigint() & !mask.clone()) | mask;
+
+    Felt::from(new_key)
+}
+
+fn merge_storage_proofs(proofs: Vec<PathfinderProof>) -> PathfinderProof {
     let class_commitment = proofs[0].class_commitment;
     let state_commitment = proofs[0].state_commitment;
     let contract_proof = proofs[0].contract_proof.clone();
@@ -216,7 +312,7 @@ pub(crate) struct PathfinderClassProof {
 
 impl PathfinderClassProof {
     /// Verifies that the class proof is valid.
-    pub(crate) fn verify(&self, class_hash: Felt) -> Result<(), String> {
+    pub(crate) fn verify(&self, class_hash: Felt) -> Result<(), ProofVerificationError> {
         verify_proof::<PoseidonHash>(class_hash, self.class_commitment, &self.class_proof)
     }
 }
@@ -254,9 +350,22 @@ pub(crate) async fn get_class_proofs(
     Ok(proofs)
 }
 
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum ProofVerificationError<'a> {
+    #[error("Proof verification failed for key {}. Proof stopped at height {}.", key.to_hex_string(), height.0)]
+    KeyNotInProof { key: Felt, height: Height, proof: &'a [TrieNode] },
+
+    #[error("Proof verification failed, node_hash {node_hash:x} != parent_hash {parent_hash:x}")]
+    InvalidChildNodeHash { node_hash: Felt, parent_hash: Felt },
+}
+
 /// This function goes through the tree from top to bottom and verifies that
 /// the hash of each node is equal to the corresponding hash in the parent node.
-pub(crate) fn verify_proof<H: HashFunctionType>(key: Felt, commitment: Felt, proof: &[TrieNode]) -> Result<(), String> {
+pub(crate) fn verify_proof<H: HashFunctionType>(
+    key: Felt,
+    commitment: Felt,
+    proof: &[TrieNode],
+) -> Result<(), ProofVerificationError> {
     let bits = key.to_bits_be();
 
     let mut parent_hash = commitment;
@@ -269,28 +378,25 @@ pub(crate) fn verify_proof<H: HashFunctionType>(key: Felt, commitment: Felt, pro
     loop {
         match trie_node_iter.next() {
             None => {
-                if index - start != 251 {
-                    return Err(format!("Proof verification failed, proof height ({}) is not 251", (index - start)));
+                if index - start != DEFAULT_STORAGE_TREE_HEIGHT {
+                    return Err(ProofVerificationError::KeyNotInProof { key, height: Height(index - start), proof });
                 }
                 break;
             }
             Some(node) => {
                 let node_hash = node.hash::<H>();
                 if node_hash != parent_hash {
-                    return Err(format!(
-                        "Proof verification failed, node_hash {:x} != parent_hash {:x}",
-                        node_hash, parent_hash
-                    ));
+                    return Err(ProofVerificationError::InvalidChildNodeHash { node_hash, parent_hash });
                 }
 
                 match node {
                     TrieNode::Binary { left, right } => {
-                        parent_hash = if bits[index] { *right } else { *left };
+                        parent_hash = if bits[index as usize] { *right } else { *left };
                         index += 1;
                     }
                     TrieNode::Edge { child, path } => {
                         parent_hash = *child;
-                        index += path.len as usize;
+                        index += path.len;
                     }
                 }
             }
@@ -310,5 +416,23 @@ pub(crate) fn get_starknet_version(block_with_txs: &BlockWithTxs) -> blockifier:
         other => {
             unimplemented!("Unsupported Starknet version: {}", other)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_modified_key() {
+        let key = Felt::from_hex_unchecked("0x281a73a6708b6c7df8ab477abbf99929586bd745d4ed2e45190d6d5edff1a6c");
+        let expected_modified_key =
+            Felt::from_hex_unchecked("0x2c1a73a6708b6c7df8ab477abbf99929586bd745d4ed2e45190d6d5edff1a6c");
+
+        let height = Height(5);
+        let edge_path = EdgePath { len: 1, value: Felt::from(0x1) };
+
+        let modified_key = get_key_following_edge(key, height, &edge_path);
+        assert_eq!(modified_key, expected_modified_key);
     }
 }
--- crates/bin/prove_block/tests/prove_block.rs
+++ crates/bin/prove_block/tests/prove_block.rs
@@ -18,6 +18,7 @@ use rstest::rstest;
 #[case::failing_assert_on_versioned_constants_2(124533)]
 #[case::fix_diff_assert_values_in_contract_subcall(87019)]
 #[case::invoke_with_replace_class(90000)]
+#[case::write_to_zero_with_edge_node(125622)]
 #[ignore = "Requires a running Pathfinder node"]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_prove_selected_blocks(#[case] block_number: u64) {
