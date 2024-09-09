175ab719074b491ce35e123599b1cab48155d2ad
Stephen Shelton <steve@brewcraft.org>
2024-09-09 09:40:04 -0600

    Oops, wrong block number

--- crates/bin/prove_block/tests/prove_block.rs
+++ crates/bin/prove_block/tests/prove_block.rs
@@ -10,7 +10,7 @@ use rstest::rstest;
 // # * 87019: diff assert values in contract subcall
 // # * 90000: one of the subcalls results in a call to `replace_class()`.
 // # * 87041: block with nonce bump inconsistency
-// # * 66645 / 66777: Blob DA blocks
+// # * 66645 / 66776: Blob DA blocks
 #[rstest]
 #[case::small_block_with_only_invoke_txs(76793)]
 #[case::additional_basic_blocks_1(76766)]
@@ -25,7 +25,7 @@ use rstest::rstest;
 #[case::invoke_with_call_to_deploy_syscall(124534)]
 #[case::block_with_nonce_bump_inconsistency(87041)]
 #[case::block_with_blob_da_1(66645)]
-#[case::block_with_blob_da_2(66777)]
+#[case::block_with_blob_da_2(66776)]
 #[ignore = "Requires a running Pathfinder node"]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_prove_selected_blocks(#[case] block_number: u64) {
