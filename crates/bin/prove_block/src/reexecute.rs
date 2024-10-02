0c13f1c620f9218400488febce89d60fb933efd0
geoffatmsl <geoffrey@moonsonglabs.com>
2024-10-02 17:39:46 +0300

    fix: handle contract commitments for contract without data items (#388)
    * fix: handle contract commitments for contract without data items
    
    Problem: reexecution panics when a contract doesnt have data items
    
    Solution: handle the edge case to prevent panics
    
    * fix: only one commitment return

--- crates/bin/prove_block/src/reexecute.rs
+++ crates/bin/prove_block/src/reexecute.rs
@@ -146,21 +146,25 @@ pub(crate) fn format_commitment_facts<H: HashFunctionType>(
 
 impl PerContractStorage for ProverPerContractStorage {
     async fn compute_commitment(&mut self) -> Result<CommitmentInfo, CommitmentInfoError> {
-        // TODO: error code
-        let contract_data =
-            self.storage_proof.contract_data.as_ref().expect("storage proof should have a contract_data field");
-        let updated_root = contract_data.root;
+        let (updated_root, commitment_facts) = match self.storage_proof.contract_data.as_ref() {
+            Some(contract_data) => {
+                let updated_root = contract_data.root;
+                let commitment_facts = format_commitment_facts::<PedersenHash>(&contract_data.storage_proofs);
 
-        let commitment_facts = format_commitment_facts::<PedersenHash>(&contract_data.storage_proofs);
+                let previous_commitment_facts = match &self.previous_storage_proof.contract_data {
+                    None => HashMap::default(),
+                    Some(previous_contract_data) => {
+                        format_commitment_facts::<PedersenHash>(&previous_contract_data.storage_proofs)
+                    }
+                };
 
-        let previous_commitment_facts = match &self.previous_storage_proof.contract_data {
-            None => HashMap::default(),
-            Some(previous_contract_data) => {
-                format_commitment_facts::<PedersenHash>(&previous_contract_data.storage_proofs)
+                let commitment_facts =
+                    commitment_facts.into_iter().chain(previous_commitment_facts.into_iter()).collect();
+                (updated_root, commitment_facts)
             }
+            // Defaults when no contract data is available
+            None => (Felt252::ZERO, HashMap::default()),
         };
-
-        let commitment_facts = commitment_facts.into_iter().chain(previous_commitment_facts.into_iter()).collect();
 
         Ok(CommitmentInfo {
             previous_root: self.previous_tree_root,
