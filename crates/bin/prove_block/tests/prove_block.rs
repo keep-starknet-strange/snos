6dc2c3b37feab9c7225f2b6f11d2d0a73567f731
ftheirs <fntheirs@gmail.com>
2024-10-09 12:59:08 -0300

    add block to CI

--- crates/bin/prove_block/tests/prove_block.rs
+++ crates/bin/prove_block/tests/prove_block.rs
@@ -53,6 +53,8 @@ use rstest::rstest;
 #[case::key_not_in_proof_0(155087)]
 #[case::key_not_in_proof_1(162388)]
 #[case::key_not_in_proof_2(155172)]
+#[case::key_not_in_proof_3(156855)]
+#[case::key_not_in_proof_4(174968)]
 #[ignore = "Requires a running Pathfinder node"]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_prove_selected_blocks(#[case] block_number: u64) {
