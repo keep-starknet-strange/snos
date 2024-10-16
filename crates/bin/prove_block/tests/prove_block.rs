eb0c4f531bcf1c853204e817bc28a6528c9bbb41
Herman Obst Demaestri <70286869+HermanObst@users.noreply.github.com>
2024-10-15 22:03:50 -0300

    fix: timestamp and block number mismatch (#401)
    * fix: timestamp and block number mismatch
    
    * experiments
    
    * fetch Block Timestamp and Block Number from ExecutionInfo
    
    * fmt + clippy
    
    * fmt + clippy
    
    * remove unused code
    
    * add integration tests
    
    * fix clippy
    
    * Update crates/starknet-os/src/execution/deprecated_syscall_handler.rs
    
    Co-authored-by: Stephen Shelton <steve@brewcraft.org>
    
    * ptr instead of pointer
    
    * restore tx version to zero
    
    ---------
    
    Co-authored-by: Stephen Shelton <steve@brewcraft.org>

--- crates/bin/prove_block/tests/prove_block.rs
+++ crates/bin/prove_block/tests/prove_block.rs
@@ -55,6 +55,8 @@ use rstest::rstest;
 #[case::key_not_in_proof_0(155087)]
 #[case::key_not_in_proof_1(162388)]
 #[case::key_not_in_proof_2(155172)]
+#[case::timestamp_rounding_1(162389)]
+#[case::timestamp_rounding_2(167815)]
 #[ignore = "Requires a running Pathfinder node"]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_prove_selected_blocks(#[case] block_number: u64) {
