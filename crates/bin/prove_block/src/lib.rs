cf9c95f7ab5377b52fb50388c0cf3b3f37001457
Herman Obst Demaestri <hodemaestri@gmail.com>
2024-11-08 11:06:13 -0300

    remove COMPILED_OS const from lib

--- crates/bin/prove_block/src/lib.rs
+++ crates/bin/prove_block/src/lib.rs
@@ -48,8 +48,6 @@ mod state_utils;
 mod types;
 mod utils;
 
-pub const DEFAULT_COMPILED_OS: &[u8] = include_bytes!("../../../../build/os_latest.json");
-
 #[derive(Debug, Error)]
 pub enum ProveBlockError {
     #[error("RPC Error: {0}")]
