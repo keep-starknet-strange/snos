371ed6e23f1a20f768105c81919d8ff15bf1d8c4
Herman Obst Demaestri <hodemaestri@gmail.com>
2024-11-12 16:50:06 -0300

    add setup script

--- setup-scripts/setup-cairo.sh
+++ setup-scripts/setup-cairo.sh
@@ -2,7 +2,7 @@
 
 # Install Python 3.9.15 using pyenv, if not already installed
 pyenv install -s 3.9.15
-
+pyenv local 3.9.15
 # Set the Python version and create a virtual environment
 PYENV_VERSION=3.9.15 python -m venv snos-env
 
