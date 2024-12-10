436a9af57260078387a752f26cdf023bfdf96096
ftheirs <fntheirs@gmail.com>
2024-12-10 09:56:55 -0300

    include v0.13.2.1 as latest

--- crates/bin/prove_block/src/rpc_utils.rs
+++ crates/bin/prove_block/src/rpc_utils.rs
@@ -214,6 +214,7 @@ pub(crate) fn get_starknet_version(block_with_txs: &BlockWithTxs) -> blockifier:
         "0.13.1" => blockifier::versioned_constants::StarknetVersion::V0_13_1,
         "0.13.1.1" => blockifier::versioned_constants::StarknetVersion::V0_13_1_1,
         "0.13.2" => blockifier::versioned_constants::StarknetVersion::V0_13_2,
+        "0.13.2.1" => blockifier::versioned_constants::StarknetVersion::Latest,
         "0.13.3" => blockifier::versioned_constants::StarknetVersion::Latest,
         other => {
             unimplemented!("Unsupported Starknet version: {}", other)
