cad469b0650eed7763e8eedaba5c445fdfd7cfa1
mohiiit <mohitdhattarwal.personal@gmail.com>
2025-02-20 10:49:26 +0530

    chore: reverting the native fee token

--- crates/rpc-replay/src/block_context.rs
+++ crates/rpc-replay/src/block_context.rs
@@ -57,7 +57,7 @@ pub fn build_block_context(
                 "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"
             ),
             eth_fee_token_address: contract_address!(
-                "0x047adc7dee88eec362d71a52c25d40559a921434b2d90e75b6a4a6e4e9fb9ab1"
+                "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
             ),
         },
     };
