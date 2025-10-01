46b262c082c042cc9d7cea049252a0a540b65342
Prakhar Pandey <prkpandey942@gmail.com>
2025-10-01 14:56:34 +0530

    add: TODOs for handling block 0

--- crates/core/generate-pie/src/lib.rs
+++ crates/core/generate-pie/src/lib.rs
@@ -208,6 +208,7 @@ pub async fn generate_pie(input: PieGenerationInput) -> Result<PieGenerationResu
         info!("Generating cached state input for block {}", block_number);
         let mut cached_state_input = generate_cached_state_input(
             &rpc_client,
+            // TODO: Update this since it's not safe when block_number == 0
             BlockId::Number(block_number - 1),
             &block_accessed_addresses,
             &block_accessed_classes,
