0d27d70a7de20eb4ebb82f7e7c6cb3b25105f44c
Prakhar Pandey <prkpandey942@gmail.com>
2025-10-22 23:49:50 +0530

    update: async state reader to use proper runtime

--- crates/rpc-client/src/state_reader/mod.rs
+++ crates/rpc-client/src/state_reader/mod.rs
@@ -265,27 +265,22 @@ impl AsyncRpcStateReader {
 impl StateReader for AsyncRpcStateReader {
     fn get_storage_at(&self, contract_address: ContractAddress, key: StorageKey) -> StateResult<Felt> {
         execute_coroutine(self.get_storage_at_async(contract_address, key))
-            .map_err(|e| StateError::StateReadError(e.to_string()))?
     }
 
     fn get_nonce_at(&self, contract_address: ContractAddress) -> StateResult<Nonce> {
         execute_coroutine(self.get_nonce_at_async(contract_address))
-            .map_err(|e| StateError::StateReadError(e.to_string()))?
     }
 
     fn get_class_hash_at(&self, contract_address: ContractAddress) -> StateResult<ClassHash> {
         execute_coroutine(self.get_class_hash_at_async(contract_address))
-            .map_err(|e| StateError::StateReadError(e.to_string()))?
     }
 
     fn get_compiled_class(&self, class_hash: ClassHash) -> StateResult<RunnableCompiledClass> {
         execute_coroutine(self.get_compiled_class_async(class_hash))
-            .map_err(|e| StateError::StateReadError(e.to_string()))?
     }
 
     fn get_compiled_class_hash(&self, class_hash: ClassHash) -> StateResult<CompiledClassHash> {
         execute_coroutine(self.get_compiled_class_hash_async(class_hash))
-            .map_err(|e| StateError::StateReadError(e.to_string()))?
     }
 }
 
